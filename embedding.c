f857a9abe5e90c45b247939a2862ee16cfd2a808
Regis Caillaud <sidger@zoho.com>
2020-06-22 12:07:28 +0200

    Adding more functions

--- embedding.c
+++ embedding.c
@@ -3,18 +3,50 @@
 
 JULIA_DEFINE_FAST_TLS() // only define this once, in an executable (not in a shared library) if you want fast code.
 
-  void nimjl_init() {
-    jl_init();
-  }
+void nimjl_init() {jl_init();}
+void nimjl_atexit_hook(int code) {nimjl_atexit_hook(code);}
 
-jl_value_t* nimjl_eval_string(char* code) {
-  jl_eval_string(code);
-}
+jl_value_t* nimjl_eval_string(char* code) {jl_eval_string(code);}
 
-void nimjl_atexit_hook(int code) {
-  nimjl_atexit_hook(code);
-}
 
+uint8_t* nimjl_unbox_uint8 (jl_value_t* value) {jl_unbox_uint8(value);}
+uint16_t* nimjl_unbox_uint16(jl_value_t* value) {jl_unbox_uint16(value);}
+uint32_t* nimjl_unbox_uint32(jl_value_t* value) {jl_unbox_uint32(value);}
+uint64_t* nimjl_unbox_uint64(jl_value_t* value) {jl_unbox_uint64(value);}
+
+int8_t* nimjl_unbox_int8 (jl_value_t* value) {jl_unbox_int8(value);}
+int16_t* nimjl_unbox_int16(jl_value_t* value) {jl_unbox_int16(value);}
+int32_t* nimjl_unbox_int32(jl_value_t* value) {jl_unbox_int32(value);}
+int64_t* nimjl_unbox_int64(jl_value_t* value) {jl_unbox_int64(value);}
+
+float * nimjl_unbox_float32(jl_value_t* value) {jl_unbox_float32(value);}
+double * nimjl_unbox_float64(jl_value_t* value) {jl_unbox_float64(value);}
+
+
+jl_value_t* nimjl_box_uint8(uint8_t value) {jl_box_uint8(value);}
+jl_value_t* nimjl_box_uint16(uint16_t value) {jl_box_uint16(value);}
+jl_value_t* nimjl_box_uint32(uint32_t value) {jl_box_uint32(value);}
+jl_value_t* nimjl_box_uint64(uint64_t value) {jl_box_uint64(value);}
+
+jl_value_t* nimjl_box_int8(int8_t value) {jl_box_int8(value);}
+jl_value_t* nimjl_box_int16(int16_t value) {jl_box_int16(value);}
+jl_value_t* nimjl_box_int32(int32_t value) {jl_box_int32(value);}
+jl_value_t* nimjl_box_int64(int64_t value) {jl_box_int64(value);}
+
+jl_value_t* nimjl_box_float32(float value) {jl_box_float32(value);}
+jl_value_t* nimjl_box_float64(double value) {jl_box_float64(value);}
+
+
+jl_function_t *nimjl_get_function(const char *name) { return jl_get_function(jl_base_module, name);}
+jl_value_t *nimjl_call(jl_function_t *f, jl_value_t **args, int32_t nargs) { jl_call(f, args, nargs);}
+
+// jl_value_t *jl_call0(jl_function_t *f);
+// jl_value_t *jl_call1(jl_function_t *f, jl_value_t *a);
+// jl_value_t *jl_call2(jl_function_t *f, jl_value_t *a, jl_value_t *b);
+// jl_value_t *jl_call3(jl_function_t *f, jl_value_t *a, jl_value_t *b, jl_value_t *c);
+
+
+/*
 double nimjl_unbox_float64(jl_value_t* value) {
   if(jl_typeis(value, jl_float64_type))
   {
@@ -25,6 +57,106 @@ double nimjl_unbox_float64(jl_value_t* value) {
     printf("ERROR: expected type float 64 code: %i but got %i instead.\n", jl_float64_type, jl_typeof(value));
   }
 }
+
+float nimjl_unbox_float32(jl_value_t* value) {
+  if(jl_typeis(value, jl_float32_type))
+  {
+    return jl_unbox_float32(value);
+  }
+  else
+  {
+    printf("ERROR: expected type float 64 code: %i but got %i instead.\n", jl_float32_type, jl_typeof(value));
+  }
+}
+
+int64_t nimjl_unbox_int64(jl_value_t* value) {
+  if(jl_typeis(value, jl_int64_type))
+  {
+    return jl_unbox_int64(value);
+  }
+  else
+  {
+    printf("ERROR: expected type int 64 code: %i but got %i instead.\n", jl_int64_type, jl_typeof(value));
+  }
+}
+
+int32_t nimjl_unbox_int32(jl_value_t* value) {
+  if(jl_typeis(value, jl_int32_type))
+  {
+    return jl_unbox_int32(value);
+  }
+  else
+  {
+    printf("ERROR: expected type int 64 code: %i but got %i instead.\n", jl_int32_type, jl_typeof(value));
+  }
+}
+
+int16_t nimjl_unbox_int16(jl_value_t* value) {
+  if(jl_typeis(value, jl_int16_type))
+  {
+    return jl_unbox_int16(value);
+  }
+  else
+  {
+    printf("ERROR: expected type int 64 code: %i but got %i instead.\n", jl_int16_type, jl_typeof(value));
+  }
+}
+
+int8_t nimjl_unbox_int8(jl_value_t* value) {
+  if(jl_typeis(value, jl_int8_type))
+  {
+    return jl_unbox_int8(value);
+  }
+  else
+  {
+    printf("ERROR: expected type int 64 code: %i but got %i instead.\n", jl_int8_type, jl_typeof(value));
+  }
+}
+
+uint64_t nimjl_unbox_uint64(jl_value_t* value) {
+  if(jl_typeis(value, jl_uint64_type))
+  {
+    return jl_unbox_uint64(value);
+  }
+  else
+  {
+    pruintf("ERROR: expected type uint 64 code: %i but got %i instead.\n", jl_uint64_type, jl_typeof(value));
+  }
+}
+
+uint32_t nimjl_unbox_uint32(jl_value_t* value) {
+  if(jl_typeis(value, jl_uint32_type))
+  {
+    return jl_unbox_uint32(value);
+  }
+  else
+  {
+    pruintf("ERROR: expected type uint 64 code: %i but got %i instead.\n", jl_uint32_type, jl_typeof(value));
+  }
+}
+
+uint16_t nimjl_unbox_uint16(jl_value_t* value) {
+  if(jl_typeis(value, jl_uint16_type))
+  {
+    return jl_unbox_uint16(value);
+  }
+  else
+  {
+    pruintf("ERROR: expected type uint 64 code: %i but got %i instead.\n", jl_uint16_type, jl_typeof(value));
+  }
+}
+
+uint8_t nimjl_unbox_uint8(jl_value_t* value) {
+  if(jl_typeis(value, jl_uint8_type))
+  {
+    return jl_unbox_uint8(value);
+  }
+  else
+  {
+    pruintf("ERROR: expected type uint 64 code: %i but got %i instead.\n", jl_uint8_type, jl_typeof(value));
+  }
+}
+*/
 
 //int main(int argc, char *argv[])
 //{
--- embedding.nim
+++ embedding.nim
@@ -1,20 +1,60 @@
 {.compile:"embedding.c", passc:"-DJULIA_ENABLE_THREADING=1", passc:"-I/home/rcaillaud/julia-1.4.2/include/julia", passL:"-lm" passL:"-Wl,-rpath,/home/rcaillaud/julia-1.4.2/lib/" passL:"-Wl,-rpath,/home/rcaillaud/julia-1.4.2/lib/julia" passL:"-ljulia".}
 
 type nimjl_value {.importc: "jl_value_t*", header: "julia.h".} = distinct pointer
+type nimjl_func {.importc: "jl_function_t *", header: "julia.h".} = distinct pointer
+type nimjl_module{.importc: "jl_module_t *", header: "julia.h".} = distinct pointer
+
 proc nimjl_init*() {.importc.}
 proc nimjl_eval_string*(code: cstring) : nimjl_value {.importc.}
+
 proc nimjl_atexit_hook*(exit_code : cint) {.importc.}
+
 proc nimjl_unbox_float64*(value: nimjl_value) : float64 {.importc.}
+proc nimjl_unbox_float32*(value: nimjl_value) : float32 {.importc.}
+
+proc nimjl_unbox_int64*(value: nimjl_value)   : int64   {.importc.}
+proc nimjl_unbox_int32*(value: nimjl_value)   : int32   {.importc.}
+proc nimjl_unbox_int16*(value: nimjl_value)   : int16   {.importc.}
+proc nimjl_unbox_int8* (value: nimjl_value)   : int8    {.importc.}
+
+proc nimjl_unbox_uint64*(value: nimjl_value)  : uint64  {.importc.}
+proc nimjl_unbox_uint32*(value: nimjl_value)  : uint32  {.importc.}
+proc nimjl_unbox_uint16*(value: nimjl_value)  : uint16  {.importc.}
+proc nimjl_unbox_uint8* (value: nimjl_value)  : uint8   {.importc.}
+
+proc nimjl_box_float64* (value: float64)  : nimjl_value  {.importc.}
+proc nimjl_box_float32* (value: float32)  : nimjl_value  {.importc.}
+
+proc nimjl_box_int64*   (value: int64)    : nimjl_value  {.importc.}
+proc nimjl_box_int32*   (value: int32)    : nimjl_value  {.importc.}
+proc nimjl_box_int16*   (value: int16)    : nimjl_value  {.importc.}
+proc nimjl_box_int8*    (value: int8)     : nimjl_value  {.importc.}
+
+proc nimjl_box_uint64*  (value: uint64)   : nimjl_value  {.importc.}
+proc nimjl_box_uint32*  (value: uint32)   : nimjl_value  {.importc.}
+proc nimjl_box_uint16*  (value: uint16)   : nimjl_value  {.importc.}
+proc nimjl_box_uint8*   (value: uint8)    : nimjl_value  {.importc.}
+
+proc nimjl_get_function*(name: cstring): nimjl_func {.importc.}
+proc nimjl_call*(function: nimjl_func, values: ptr nimjl_value, nargs: cint): nimjl_value {.importc.}
 
 echo "init"
 nimjl_init()
-echo "eval"
-discard nimjl_eval_string("print(sqrt(2.0))")
-echo ""
-var test = nimjl_eval_string("sqrt(4.0)")
-echo type(test)
-echo test.repr
-echo nimjl_unbox_float64(test)
+
+block:
+  echo "eval_string"
+  discard nimjl_eval_string("print(sqrt(2.0))")
+  var test = nimjl_eval_string("sqrt(4.0)")
+  echo nimjl_unbox_float64(test)
+
+block:
+  echo "jl_call"
+  var x : nimjl_value = nimjl_box_float64(4.0)
+  var f = nimjl_get_function("sqrt");
+  var res = nimjl_call(f, x.addr, 1.cint)
+  echo nimjl_unbox_int32(res)
+  echo nimjl_unbox_float64(x)
+
 echo "exithook"
 ## atexit cause stack overflow ?
 #nimjl_atexit_hook(0)
