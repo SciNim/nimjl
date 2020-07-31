1f4e65779a2a93dc82379a6b20705429d63b1f14
Regis Caillaud <sidger@zoho.com>
2020-07-31 14:10:28 +0200

    format some code

--- tests/test1.nim
+++ tests/test1.nim
@@ -42,29 +42,26 @@ test "jl_call1":
 test "jl_array_1d":
   let ARRAY_LEN = 10
   var orig : seq[float64] = @[0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0]
-  let unchecked_orig = cast[ptr UncheckedArray[float64]](orig[0].addr)
 
   var array_type : nimjl_value  = nimjl_apply_array_type_float64(1)
   var x = nimjl_alloc_array_1d(array_type, ARRAY_LEN.csize_t)
 
   nimjl_gc_push1(x.addr)
   var xData : ptr UncheckedArray[float64] = cast[ptr UncheckedArray[float64]](nimjl_array_data(x))
-
   check ARRAY_LEN == nimjl_array_len(x)
 
   for i in 0..<nimjl_array_len(x):
     xData[i] = i.float64
 
-
   var reverse = nimjl_get_function(jl_base_module, "reverse!")
   var res = nimjl_call(reverse, x.addr, 1)
-  var resData = cast[ptr UncheckedArray[float64]](nimjl_array_data(x))
+  check not isNil(res)
 
+  var resData = cast[ptr UncheckedArray[float64]](nimjl_array_data(x))
   check resData == xData
 
   for i in 0..<ARRAY_LEN:
     check xData[i] == orig[ARRAY_LEN - i - 1]
-
   nimjl_gc_pop()
 
 test "jl_array_1d_own_buffer":
@@ -74,17 +71,16 @@ test "jl_array_1d_own_buffer":
 
   var array_type : nimjl_value  = nimjl_apply_array_type_float64(1)
   var x = nimjl_ptr_to_array_1d(array_type, unchecked_orig, ARRAY_LEN.csize_t, 1)
-
   check ARRAY_LEN == nimjl_array_len(x)
 
   for i in 0..<nimjl_array_len(x):
     unchecked_orig[i] = i.float64
 
-
   var reverse = nimjl_get_function(jl_base_module, "reverse!")
   var res = nimjl_call(reverse, x.addr, 1)
-  var resData = cast[ptr UncheckedArray[float64]](nimjl_array_data(x))
+  check not isNil(res)
 
+  var resData = cast[ptr UncheckedArray[float64]](nimjl_array_data(x))
   check resData == unchecked_orig
 
   for i in 0..<ARRAY_LEN:
@@ -93,12 +89,14 @@ test "jl_array_1d_own_buffer":
 test "external_module":
   var res_eval_include = nimjl_eval_string("include(\"tests/test.jl\")")
   check not isNil(res_eval_include)
+
   var res_eval_using = nimjl_eval_string("using .custom_module")
   check not isNil(res_eval_using)
 
 test "dummy":
   var dummy = nimjl_get_function(jl_main_module, "dummy")
   check not isNil(dummy)
+
   var ret : nimjl_value = nimjl_call0(dummy)
   check not isNil(ret)
 
@@ -151,7 +149,6 @@ test "external_module : mutateMeBaby[Array]":
 
 test "external_module : testMeBaby[Tensor]":
   var testMeBaby = nimjl_get_function(jl_main_module, "testMeBaby")
-  echo testMeBaby.repr
   check not isNil(testMeBaby)
 
   var dims: seq[int] = @[4, 6, 8]
@@ -183,7 +180,7 @@ test "external_module : testMeBaby[Tensor]":
 
   nimjl_gc_pop()
 
-test "external_module : mutateMeBaby[Array]":
+test "external_module : mutateMeBaby[Tensor]":
   var mutateMeBaby = nimjl_get_function(jl_main_module, "mutateMeBaby!")
   check not isNil(mutateMeBaby)
   var testMeBaby = nimjl_get_function(jl_main_module, "testMeBaby")
