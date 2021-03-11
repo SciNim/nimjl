d875b4ddf7c104f83cb8a4382d392cd2427aa09f
Clonkk <sidger@zoho.com>
2021-03-11 11:34:23 +0100

    improve in readme, marginal change in config

--- nimjl/config.nim
+++ nimjl/config.nim
@@ -7,6 +7,10 @@ const juliaHeader* = "julia.h"
 const juliaLibPath* = juliaPath / "lib"
 const juliaDepPath* = juliaPath / "lib" / "julia"
 
+const libPrefix = "lib"
+const libSuffix = ".so"
+const juliaLibName* = juliaLibPath / libPrefix & "julia" & libSuffix
+
 # TODO: handle more platform
 {.passC: " -DJULIA_ENABLE_THREADING=1".}
 {.passC: "-I" & juliaIncludesPath.}
@@ -14,8 +18,7 @@ const juliaDepPath* = juliaPath / "lib" / "julia"
 {.passL: "-Wl,-rpath," & juliaLibPath.}
 {.passL: "-L" & juliaDepPath.}
 {.passL: "-Wl,-rpath," & juliaDepPath.}
-{.passL: "-ljulia".}
-
+{.link: juliaLibName}
 # static:
 #   echo "juliaPath> ", juliaPath
 #   echo "juliaIncludesPath> ", juliaIncludesPath
