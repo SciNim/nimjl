b85ea501f71a2d86dea9d91f4948cc1474e92bbc
Clonkk <sidger@zoho.com>
2022-06-08 16:24:36 +0200

    clean dumdum

--- dumdum.nim
+++ dumdum.nim
@@ -7,21 +7,9 @@ let
 
 Julia.init(1):
   Pkg:
-    add(pkgname1, pkgversion)
-    add(pkgname2)
+    # add(pkgname1, pkgversion)
+    # add(pkgname2)
+    add(name=pkgname1, version=pkgversion)
+    add(name=pkgname2)
 
-# import nimjl
-# template init(body : untyped) =
-#   var storeMe: seq[tuple[name, version: string]]
-#   proc add(name: string, version: string) =
-#     let s = (name: name, version: version,)
-#     echo s
-#     storeMe.add(s)
-#   body
-#
-# proc main() =
-#   init:
-#     add("example", "1.2.1")
-#
-# main()
 
